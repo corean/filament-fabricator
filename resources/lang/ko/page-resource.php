5c5bd59841d949a85247ff931292883686a56fba
corean <corean@corean.biz>
2024-04-04 23:56:03 +0900

    - Korean translations add

---
+++ resources/lang/ko/page-resource.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'labels' => [
+        'blocks' => '블록',
+        'layout' => '레이아웃',
+        'page' => '페이지',
+        'pages' => '페이지',
+        'parent' => '상위',
+        'slug' => '슬러그',
+        'title' => '제목',
+        'url' => 'URL',
+    ],
+
+    'errors' => [
+        'slug_starts_or_ends_with_slash' => '슬러그는 슬래시로 시작하거나 끝날 수 없습니다.',
+    ],
+
+    'actions' => [
+        'save' => '저장',
+        'visit' => '방문',
+    ],
+];
